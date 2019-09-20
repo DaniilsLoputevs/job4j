6107b3c25a4a7af079c1def70f4d431055d4d59c
DaniilsLoputevs <laiwiense@gmail.com>
2019-09-20 12:44:47 +0100

    Modified Max v1

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -2,13 +2,7 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        int result;
-        if (left > right) {
-            result = left;
-        } else {
-            result = right;
-        }
+        return left > right ? left : right;
         // if both numbers are equals, result = one of them
-        return result;
     }
 }
