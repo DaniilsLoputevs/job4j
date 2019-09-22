0172adf1ee9cd45268a0874ea5046dbb33c8a68d
DaniilsLoputevs <laiwiense@gmail.com>
2019-09-22 19:53:08 +0100

    update CheckPrimeNumber v2

--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,28 +3,11 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
     public boolean check(int num) {
         boolean prime = true;
-            if (num <= 10) {
-                if ( num == 1) {
-                    prime = true;
-                }else if (num == 2) {
-                    prime = true;
-                } else if (num == 3) {
-                    prime = true;
-                } else if (num == 5) {
-                    prime = true;
-                } else if (num == 7) {
-                    prime = true;
-                } else prime = false;
-                return prime;
-            }
-            if (num % 2 == 0) {
-                prime = false;
-            } else if (num % 3 == 0) {
-                prime = false;
-            } else if (num % 5 == 0) {
-                prime = false;
-            } else if (num % 7 == 0) {
-                prime = false;
+            for (int j = 2; j < num; j++) {
+                if (num % j == 0) {
+                    prime = false;
+                    break;
+                }
             }
         return prime;
     }
