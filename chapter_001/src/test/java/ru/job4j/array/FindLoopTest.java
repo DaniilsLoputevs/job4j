4e895cef46599a1bb0b1c7f45e0f93499349fb02
DaniilsLoputevs <laiwiense@gmail.com>
2019-09-24 16:18:37 +0100

    update FindLoop for task 6.5

--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -8,20 +8,40 @@ public class FindLoopTest {
 
     @Test // search value = 5 >> find index = 0
     public void whenArrayHas5Then0() {
-        FindLoop find = new FindLoop();
         int[] input = new int[] {5, 10, 3};
         int value = 5;
-        int result = find.indexOf(input, value);
+        int result = FindLoop.indexOf(input, value);
         int expect = 0;
         assertThat(result, is(expect));
     }
     @Test // search value = 7 >> find index = -1
     public void whenArrayHas7Then_Nothing () {
-        FindLoop find = new FindLoop();
         int[] input = new int[] {5, 10, 3};
         int value = 7;
-        int result = find.indexOf(input, value);
+        int result = FindLoop.indexOf(input, value);
         int expect = -1;
+        assertThat(result, is(expect));
+    }
+
+
+    @Test // search value = 2 >> find index = 3
+    public void whenFind3() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
+    @Test // search value = 2 >> find index = 3
+    public void whenFind7() {
+        int[] input = new int[] {5, 2, 10, 2, 4, 7, 9, 0, 1, 5, 10, 7};
+        int value = 7;
+        int start = 2;
+        int finish = 6;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 5;
         assertThat(result, is(expect));
     }
 }
