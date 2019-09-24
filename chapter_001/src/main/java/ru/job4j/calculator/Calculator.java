c3dfb58b0219af8605976af989045db5004fe8e9
DaniilsLoputevs <laiwiense@gmail.com>
2019-09-24 12:56:21 +0100

    add JavaDoc for Calculator & package-info

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,22 +1,47 @@
 package ru.job4j.calculator;
-
+/**
+ * Class класс для вычесления арифметических операций + - * / ^.
+ * @author Daniil Loputevs
+ * @version v1.0
+ * @since 24.09.2019
+ */
 public class Calculator {
-
+    /**
+     * Сложение
+     * @param first перывй аргумент
+     * @param second второй аргумент
+     * @return результат
+     */
     public static void add(double first, double second) {
         double result =  first + second;
         System.out.println(first + "+" + second + " = " + result);
     }
-
+    /**
+     * Деление
+     * @param first перывй аргумент
+     * @param second второй аргумент
+     * @return результат
+     */
     public static void div(double first, double second) {
         double result =  first / second;
         System.out.println(first + "/" + second + " = " + result);
     }
-
+    /**
+     * Умножение
+     * @param first перывй аргумент
+     * @param second второй аргумент
+     * @return результат
+     */
     public static void multiply(double first, double second) {
         double result =  first * second;
         System.out.println(first + "*" + second + " = " + result);
     }
-
+    /**
+     * Вычитание
+     * @param first перывй аргумент
+     * @param second второй аргумент
+     * @return результат
+     */
     public static void subtract(double first, double second) {
         double result =  first - second;
         System.out.println(first + "-" + second + " = " + result);
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,8 @@
+package ru.job4j.calculator;
+/**
+ * Package for calculate task.
+ *
+ * @author Daniil Loputevs
+ * @version v1.0
+ * @since 24.09.2019
+ */
