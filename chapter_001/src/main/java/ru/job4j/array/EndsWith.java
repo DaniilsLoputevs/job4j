99c24bd6db0ab6a49397a491ec78eadfe09b6832
DaniilsLoputevs <laiwiense@gmail.com>
2019-09-24 16:05:28 +0100

    add EndsWith

---
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -0,0 +1,18 @@
+package ru.job4j.array;
+
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] post) {
+        boolean result = false;
+        // проверить. что массив word имеет последние элементы одинаковые с post
+        for (int i = 1; i < post.length; i++) {
+            int index = word.length - i;
+            if (word[index] == post[i]) {
+                result = true;
+            } else {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
