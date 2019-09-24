5c5d17ddaeca2d1d93a213c351c976d76b7d5bc0
DaniilsLoputevs <laiwiense@gmail.com>
2019-09-24 14:54:52 +0100

    update Turn (array) - v1

--- chapter_001/src/main/java/ru/job4j/array/Turn.java
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -4,12 +4,9 @@ public class Turn {
     public int[] back(int[] array) {
         int temp = 0;
         int endIndex = 0;
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length / 2; i++) {
             temp = array[i];
             endIndex = (array.length - 1) - i;
-            if (i > endIndex) {
-                break;
-            }
             array[i] = array[endIndex];
             array[endIndex] = temp;
         }
