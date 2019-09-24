2aafc6702c580a87e0ccdaa3e6ebb8ad43a2c411
DaniilsLoputevs <laiwiense@gmail.com>
2019-09-24 15:48:05 +0100

    update Check - v1

--- chapter_001/src/test/java/ru/job4j/array/CheckTest.java
+++ chapter_001/src/test/java/ru/job4j/array/CheckTest.java
@@ -5,7 +5,7 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class CheckTest {
-    @Test
+    @Test // 3 elements = true
     public void whenDataMonoByTrueThenTrue() {
         Check check = new Check();
         boolean[] input = new boolean[] {true, true, true};
@@ -13,10 +13,17 @@ public class CheckTest {
         assertThat(result, is(true));
     }
 
-    @Test
+    @Test // 3 elements = false
     public void whenDataNotMonoByTrueThenFalse() {
         Check check = new Check();
         boolean[] input = new boolean[] {true, false, true};
+        boolean result = check.mono(input);
+        assertThat(result, is(false));
+    }
+    @Test // 8 elements = false
+    public void whenDataNotMono() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {true, true, true, true, false, true, true, true};
         boolean result = check.mono(input);
         assertThat(result, is(false));
     }
