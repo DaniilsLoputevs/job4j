12f29b830a3adae3672feeddcb136e5d6b242bbf
DaniilsLoputevs <laiwiense@gmail.com>
2019-10-06 16:18:20 +0100

    add Battery - task 1.7

---
+++ chapter_002/src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,30 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int value;
+    private int limit;
+
+    public Battery(int size, int limit) {
+        this.value = size;
+        this.limit = limit;
+    }
+    public void load(Battery another) {
+        int totalValue = this.value + another.value;
+        if(totalValue >= another.limit) {
+            this.value = limit;
+            another.value = totalValue - limit;
+        } else {
+            this.value = this.value + another.value;
+            another.value = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Battery first = new Battery(10000, 10000);
+        Battery second = new Battery(50, 750);
+        System.out.println("first : " + first.value + "/" + first.limit + " --- second : " + second.value + "/" + second.limit);
+        second.load(first);
+        System.out.println("first : " + first.value + "/" + first.limit + " --- second : " + second.value + "/" + second.limit);
+
+    }
+}
--- chapter_002/src/main/java/ru/job4j/oop/Student.java
+++ chapter_002/src/main/java/ru/job4j/oop/Student.java
@@ -3,11 +3,12 @@ package ru.job4j.oop;
 public class Student {
     public static void main(String[] args) {
         Student petya = new Student();
-        petya.music();
-        petya.music();
-        petya.music();
+        Student vasja = new Student();
+        String song = "I believe I can fly";
+
+        vasja.music(song);
     }
-    public void music() {
-        System.out.println("I believe I can fly");
+    public void music(String lyrics) {
+        System.out.println("I can sign a song : " + lyrics);
     }
 }
