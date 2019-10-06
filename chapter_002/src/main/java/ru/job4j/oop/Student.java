ab69ae84e29cb93c8e0af05c8432c90efcbfa3a8
DaniilsLoputevs <laiwiense@gmail.com>
2019-10-06 13:54:57 +0100

    add Student - task 1.2

---
+++ chapter_002/src/main/java/ru/job4j/oop/Student.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop;
+
+public class Student {
+    public static void main(String[] args) {
+        Student petya = new Student();
+        petya.music();
+        petya.music();
+        petya.music();
+    }
+    public void music() {
+        System.out.println("I believe I can fly");
+    }
+}
