6315caa6ebe1a3c822d71658f51b06e3491cba88
DaniilsLoputevs <laiwiense@gmail.com>
2019-10-10 22:16:40 +0100

    add package tracker

---
+++ chapter_002/src/main/java/ru/job4j/oop/tracker/Animal.java
@@ -0,0 +1,14 @@
+package ru.job4j.oop.tracker;
+
+public class Animal {
+
+    public Animal() {
+        super();
+        System.out.println("load Animal");
+    }
+
+    public static void main(String[] args) {
+        Tiger test = new Tiger();
+    }
+
+}
---
+++ chapter_002/src/main/java/ru/job4j/oop/tracker/Tiger.java
@@ -0,0 +1,10 @@
+package ru.job4j.oop.tracker;
+
+public class Tiger extends Predator {
+
+    public Tiger() {
+        super();
+        System.out.println("load Tiger");
+    }
+
+}
