5cd0fdb830d288b748db30d32351fdf9c9e1dc02
DaniilsLoputevs <laiwiense@gmail.com>
2019-10-19 13:41:07 +0100

    add Transport + Bus - task 3

---
+++ chapter_002/src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,33 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport{
+    private int passengers;
+    private double fuel;
+
+    @Override
+    public void move() {
+        System.out.println("Следующая остановка, Краснодар!");
+    }
+
+    @Override
+    public void passengers(int passengers) {
+        System.out.println("Пассажиров: " + passengers);
+        this.passengers = passengers;
+    }
+
+    @Override
+    public double fillUp(double cost) {
+        this.fuel = cost / 103; // Стоимость бензина
+        System.out.println("Вы заправились на рублей " + cost + " = " + fuel + " литров");
+        return cost;
+    }
+
+    public static void main(String[] args) {
+        Bus ikarus = new Bus();
+        ikarus.fillUp(20000);
+        ikarus.passengers(40);
+        ikarus.move();
+    }
+}
+
+
