3c57ee761553c60f48a456d134149548ba210e43
DaniilsLoputevs <laiwiense@gmail.com>
2019-11-01 22:34:58 +0000

    Глава 2 - Контрольные вопросы.[#179177] Сдача в кофе машине.

---
+++ chapter_002/src/main/java/ru/job4j/sort/Machine.java
@@ -0,0 +1,20 @@
+package ru.job4j.sort;
+
+import java.util.Arrays;
+
+public class Machine {
+    private final int[] COINS = {10, 5, 2, 1};
+
+    public int[] change(int money, int price) {
+        int[] rsl = new int[100];
+        int size = 0;
+        int change = money - price;
+        for (int i = 0; change >= 1; i++) {
+            while (change >= COINS[i]) {
+                rsl[size++] = COINS[i];
+                change -= COINS[i];
+            }
+        }
+        return Arrays.copyOf(rsl, size);
+    }
+}
