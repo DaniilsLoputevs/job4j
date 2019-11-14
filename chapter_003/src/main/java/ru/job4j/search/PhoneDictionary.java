e48c191d3984d64da05be11e64a0969961c122ef
DaniilsLoputevs <laiwiense@gmail.com>
2019-11-14 16:00:43 +0000

    1. Телефонный справочник на базе ArrayList[#179100]

--- chapter_003/src/main/java/ru/job4j/search/PhoneDictionary.java
+++ chapter_003/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -18,7 +18,7 @@ public class PhoneDictionary {
     public List<Person> find(String key) {
         List<Person> result = new ArrayList<>();
         for (Person person : persons) {
-            if (        person.getName().matches(key) || person.getSurname().matches(key)
+            if (person.getName().matches(key) || person.getSurname().matches(key)
                     || person.getPhone().matches(key) || person.getAddress().matches(key)) {
                 result.add(person);
             }
