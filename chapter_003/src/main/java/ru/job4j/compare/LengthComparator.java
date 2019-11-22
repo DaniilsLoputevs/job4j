c26ee95114bf2643f024146f1e3ac9c98be0a00e
DaniilsLoputevs <laiwiense@gmail.com>
2019-11-22 14:47:17 +0000

    2. Сортировка User с использованием Comparator[#179093]

--- chapter_003/src/main/java/ru/job4j/compare/LengthComparator.java
+++ chapter_003/src/main/java/ru/job4j/compare/LengthComparator.java
@@ -6,9 +6,9 @@ public class LengthComparator implements Comparator<User> {
 
     @Override
     public int compare(User o1, User o2) {
-        int result = (o1.getName().length() > o2.getName().length()) ? 1 : -1;
-        result = (o1.getName().length() == o2.getName().length()) ? 0 : result;
-        return result;
+        return Integer.compare(
+                o1.getName().length(),
+                o2.getName().length()
+        );
     }
-
 }
