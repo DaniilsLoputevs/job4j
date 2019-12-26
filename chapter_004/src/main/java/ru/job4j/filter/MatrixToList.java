96b5798cd67fe2f28ea88e0e0e9c291f27f18c29
DaniilsLoputevs <laiwiense@gmail.com>
2019-12-26 16:18:38 +0200

    5. Преобразование матрицы чисел в список чисел.[#178987]

--- chapter_004/src/main/java/ru/job4j/filter/MatrixToList.java
+++ chapter_004/src/main/java/ru/job4j/filter/MatrixToList.java
@@ -2,22 +2,14 @@ package ru.job4j.filter;
 
 import java.util.*;
 import java.util.ArrayList;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class MatrixToList {
 
     public static List convert(int[][] matrix) {
-
-//        return Stream.of(matrix).flatMapToInt(Arrays::stream).collect(Collectors.toList());
-
-//        return matrix.stream().flatMap(List::stream).collect(Collectors.toList());
-
-// matrix.stream().flatMap(e -> e.stream()).collect(Collectors.toList())
-
-        // заглушка
-        return new ArrayList();
-
+        List result = new ArrayList();
+        Arrays.stream(matrix)
+                .flatMapToInt(Arrays::stream)
+                .forEach(result::add);
+        return result;
     }
 }
--- chapter_004/src/test/java/ru/job4j/filter/MatrixToListTest.java
+++ chapter_004/src/test/java/ru/job4j/filter/MatrixToListTest.java
@@ -11,17 +11,11 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class MatrixToListTest {
-//    private List<List<Integer>> initCase;
     private int[][] initCase;
     private List expectedCase;
 
     @Before
     public void init() {
-//        initCase = List.of(
-//                List.of(1, 2),
-//                List.of(3, 4),
-//                List.of(5, 6)
-//        );
         initCase = new int[][] {
                 {1}, {2},
                 {3}, {4},
@@ -29,7 +23,6 @@ public class MatrixToListTest {
         expectedCase = new ArrayList<>(Arrays.asList(
                 1, 2, 3, 4, 5, 6
         ));
-
     }
 
     @Test
