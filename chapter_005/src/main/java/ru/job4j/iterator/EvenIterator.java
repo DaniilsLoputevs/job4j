482341fb75ad9d5248d74c7a885097d2b4e0c62e
DaniilsLoputevs <laiwiense@gmail.com>
2020-01-17 15:01:06 +0000

    5.1.2. Создать итератор четные числа[#219412]

--- chapter_005/src/main/java/ru/job4j/iterator/EvenIterator.java
+++ chapter_005/src/main/java/ru/job4j/iterator/EvenIterator.java
@@ -4,31 +4,61 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class EvenIterator implements Iterator {
+public class EvenIterator<Integer> implements Iterator {
     private int[] array;
     private int index = 0;
 
     public EvenIterator(int[] arr) {
         int[] buffer = new int[arr.length];
         var count = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] % 2 == 0) {
-                buffer[count] = arr[i];
+        for (int anArr : arr) {
+            if (anArr % 2 == 0) {
+                buffer[count] = anArr;
                 count++;
             }
         }
         this.array = Arrays.copyOf(buffer, count);
     }
 
-
-    @Override
+        @Override
     public boolean hasNext() {
-        return array.length > index;
+        return check() != -1;
     }
 
-    // сдесь hasNext() т.к. я не хотел повторять код, а собирался писать тоже самое.
     @Override
     public Object next() {
-        return (hasNext()) ? array[index++] : new NoSuchElementException();
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        int result = check();
+        index = (result != -1) ? index + 1 : index;
+        return result;
+
     }
+
+    private int check() {
+        var result = -1;
+        for (int i = index; i < this.array.length; i++) {
+            if (this.array[i] % 2 == 0) {
+                result = this.array[i];
+                break;
+            }
+        }
+        return result;
+    }
+
+
+    // Альтернатива
+//    @Override
+//    public boolean hasNext() {
+//        return array.length > index;
+//    }
+//
+//    // сдесь hasNext() т.к. я не хотел повторять код, а собирался писать тоже самое.
+//    @Override
+//    public Object next() {
+//        return (hasNext()) ? array[index++] : new NoSuchElementException();
+//    }
+
+
 }
