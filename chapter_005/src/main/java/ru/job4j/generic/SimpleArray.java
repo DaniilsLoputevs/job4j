0a20ebbe61cb9c3096067136a7152cf70c5b7aae
DaniilsLoputevs <laiwiense@gmail.com>
2020-01-21 14:56:13 +0000

    5.2.1. Реализовать SimpleArray<T>[#219417]

--- chapter_005/src/main/java/ru/job4j/generic/SimpleArray.java
+++ chapter_005/src/main/java/ru/job4j/generic/SimpleArray.java
@@ -1,5 +1,6 @@
 package ru.job4j.generic;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
 
@@ -7,23 +8,23 @@ public class SimpleArray<T> implements Iterable {
 
     /**
      * add(T model) - добавляет указанный элемент (model) в первую свободную ячейку;
-     * set(int index, T model) - заменяет указанным элементом (model) элемент, находящийся по индексу index;
-     * remove(int index) - удаляет элемент по указанному индексу, все находящиеся справа элементы при этом необходимо сдвинуть на единицу влево (в середине массива не должно быть пустых ячеек);
-     * get(int index) - возвращает элемент, расположенный по указанному индексу;
+     * set(int position, T model) - заменяет указанным элементом (model) элемент, находящийся по индексу position;
+     * remove(int position) - удаляет элемент по указанному индексу, все находящиеся справа элементы при этом необходимо сдвинуть на единицу влево (в середине массива не должно быть пустых ячеек);
+     * get(int position) - возвращает элемент, расположенный по указанному индексу;
      *
      * Также, реализуйте интерфейс Iterable<T> - метод iterator() возвращает итератор, предназначенный для обхода данной структуры.
      * Объект должен принимать количество ячеек. Структура не должна быть динамической. Если идет переполнение надо выкинуть ошибку.
      */
 
     private Object[] objects;
-    private int index = 0;
+    private int position = 0;
 
     public SimpleArray(int size) {
         this.objects = new Object[size];
     }
 
     public void add(T value) {
-        this.objects[index++] = value;
+        this.objects[position++] = value;
     }
     public void addAll(Collection<T> collection) {
         for (T item : collection) {
@@ -31,20 +32,28 @@ public class SimpleArray<T> implements Iterable {
         }
     }
     public void set(int index, T value) {
-        this.objects[index] = value;
+        // это должно работать в пределах добавленных элементов - Andrei
+        if (index < position) {
+            this.objects[index] = value;
+        } else {
+            System.out.println("В массиве нет такой ячейки");
+        }
     }
     public void remove(int index) {
         this.objects[index] = null;
-        for (int i = index; i < this.objects.length; i++) {
-            this.objects[index] = this.get(index + 1);
-        }
+        this.position--;
+        System.arraycopy(objects, index + 1, objects, index, position - index);
     }
     public T get(int position) {
         return (T) this.objects[position];
     }
 
+
+    public int getSize() {
+        return this.position;
+    }
     @Override
     public Iterator iterator() {
-      return new SimpleArrayIterator(objects);
+      return new SimpleArrayIterator(Arrays.copyOf(objects, position));
     }
 }
--- chapter_005/src/test/java/ru/job4j/generic/SimpleArrayTest.java
+++ chapter_005/src/test/java/ru/job4j/generic/SimpleArrayTest.java
@@ -10,11 +10,11 @@ import static org.junit.Assert.assertThat;
 
 
 public class SimpleArrayTest {
-    private SimpleArray collection;
+    private SimpleArray<String> collection;
 
     @Before
     public void init() {
-        collection = new SimpleArray<String>(10);
+        collection = new SimpleArray<>(10);
         collection.add("one");
     }
 
@@ -33,17 +33,28 @@ public class SimpleArrayTest {
     }
     @Test
     public void set() {
-        collection.set(3, "three");
-        assertThat(collection.get(3), is("three"));
+        collection.set(0, "test");
+        assertThat(collection.get(0), is("test"));
     }
     @Test
     public void remove() {
         collection.add("two");
         collection.remove(0);
         assertThat(collection.get(0), is("two"));
+        assertThat(collection.getSize(), is(1));
     }
     @Test
     public void get() {
         assertThat(collection.get(0), is("one"));
+    }
+
+    @Test
+    public void removeSize() {
+        collection.add("two");
+        collection.add("three");
+        collection.add("four");
+
+        collection.remove(1); // - two
+        assertThat(collection.getSize(), is(3));
     }
 }
