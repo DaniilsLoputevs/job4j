1b3305c44395b8c0d74ee378c8262aeb6938574e
DaniilsLoputevs <laiwiense@gmail.com>
2020-01-30 18:18:13 +0000

    1. Создать модель User[#219424]

--- chapter_005/src/main/java/ru/job4j/map/User.java
+++ chapter_005/src/main/java/ru/job4j/map/User.java
@@ -1,9 +1,47 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
+import java.util.Objects;
 
 public class User {
     String name;
     int children;
-    Calendar birthday;
+//    Calendar birthday;
+
+    public User(String name, int children) {
+        this.name = name;
+        this.children = children;
+//        this.birthday = birthday;
+    }
+
+    // Getters && Setters
+    public String getName() {
+        return name;
+    }
+    public int getChildren() {
+        return children;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setChildren(int children) {
+        this.children = children;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children);
+    }
 }
