583af3b9fada1a174fb74a1216524384dab88dc0
DaniilsLoputevs <55362320+DaniilsLoputevs@users.noreply.github.com>
2020-05-14 15:14:24 +0100

    SQL Select by company [#219387]

--- chapter_007/src/main/java/ru/job4j/scripts/companyAndPerson.sql
+++ chapter_007/src/main/java/ru/job4j/scripts/companyAndPerson.sql
@@ -1,4 +1,4 @@
-drop if exists company, person;
+drop table company, person;
 --truncate table company, person;
 
 CREATE TABLE IF NOT EXISTS company(
@@ -13,25 +13,31 @@ CREATE TABLE IF NOT EXISTS person(
 	CONSTRAINT person_pkey PRIMARY KEY (id)
 );
 
+----- TASK -----
+
 insert into company(id, name) values ('1', 'first');
 insert into company(id, name) values ('2', 'second');
 insert into company(id, name) values ('3', 'third');
+insert into company(id, name) values ('4', 'fourth');
+insert into company(id, name) values ('5', 'fifth');
 
 insert into person(id, name, company_id) values ('100', 'one', '1');
 insert into person(id, name, company_id) values ('200', 'two', '2');
 insert into person(id, name, company_id) values ('300', 'three', '3');
+insert into person(id, name, company_id) values ('400', 'four', '4');
+insert into person(id, name, company_id) values ('500', 'five', '5');
+insert into person(id, name, company_id) values ('600', 'six', '5');
 
 
--- TASK
--- 1) имя компании = человек имена.
--- все кто ВНЕ компании с id = 5.
+
+--Получить в одном запросе:
+-- имя человека, имя компании.
+-- * все кроме копании с id = 3
 select company.name, person.name
-from person join company on person.id = company.id
-group by company.name, person.name;
+from person join company on person.company_id = company.id
+where company.id != 3;
 
-
-
-
-
-
--- 2) компания с макс. людей и её численность.
+-- компании с макс кол-во человек.
+with tbl as (select company.name as c_names, count(person.company_id) as person_count
+from company, person where person.company_id = company.id group by company.name)
+select * from tbl where person_count >= all(select person_count from tbl);
