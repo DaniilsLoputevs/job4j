9d533136594dd01b971adb7d05cfe6ab68b4a974
DaniilsLoputevs <55362320+DaniilsLoputevs@users.noreply.github.com>
2020-06-25 13:11:58 +0100

    1.1. Состояние нити. [#305053]

--- chapter_010/src/main/java/ru/job4j/concurrent/ThreadState.java
+++ chapter_010/src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -10,12 +10,10 @@ public class ThreadState {
         first.start();
         System.out.println("second state: " + second.getState());
         second.start();
-        while (isThreadAlive(first) && isThreadAlive(second)) {
+        while (isThreadAlive(first) || isThreadAlive(second)) {
             System.out.println("first state: " + first.getState());
             System.out.println("second state: " + second.getState());
         }
-        System.out.println("first state: " + first.getState());
-        System.out.println("second state: " + second.getState());
     }
 
     private static boolean isThreadAlive(Thread thread) {
