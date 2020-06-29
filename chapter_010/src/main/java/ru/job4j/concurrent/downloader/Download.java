c27b552f4fee589775154524319e2850ddfc0454
DaniilsLoputevs <55362320+DaniilsLoputevs@users.noreply.github.com>
2020-06-29 22:43:35 +0100

    4. Скачивание файла с ограничением. [#305052]

--- chapter_010/src/main/java/ru/job4j/concurrent/downloader/Download.java
+++ chapter_010/src/main/java/ru/job4j/concurrent/downloader/Download.java
@@ -7,25 +7,29 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
-import java.util.Properties;
+import java.util.Map;
 
+/**
+ * expected args:
+ * -url {url value} -t {target path} -n {name} -s {num} {kof}
+ * num = "100"
+ * kof = "b"
+ */
 public class Download {
-    public static void main(String[] args) {
-        Properties config = new ArgsLambda.Builder()
-                .add("-url", arg -> arg.contains("https://"))
-                .add("-s", List.of(
-                        arg -> arg.matches("[0-9]"),
-                        arg -> arg.matches("kb") || arg.matches("b")))
-                .loadAndRun(args);
+    public static void logic(String[] args) {
+        var config = new Config().processingArgs(args);
 
+        String url = config.get("-url").get(0);
+        String target = config.get("-t").get(0);
+        String name = config.get("-n").get(0);
+        long speed = speedConvert(config.get("-s"));
 
-        String file = config.getProperty("-url");
-        try (var in = new BufferedInputStream(new URL(file).openStream());
-             var fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
-//            int speed = speedConvert(config.getProperty("-s"));
-            byte[] dataBuffer = new byte[1024];
+        try (var in = new BufferedInputStream(new URL(url).openStream());
+             var fileOutputStream = new FileOutputStream(target + '/' + name)) {
+
+            byte[] dataBuffer = new byte[(int) speed];
             int bytesRead;
-            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+            while ((bytesRead = in.read(dataBuffer, 0, (int) speed)) != -1) {
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
                 Thread.sleep(1000);
             }
@@ -34,7 +38,38 @@ public class Download {
         }
     }
 
-//    private static int speedConvert(String speed) {
-//        return null;
-//    }
+    private static long speedConvert(List<String> speed) {
+        long realSpeed = Integer.parseInt(speed.get(0));
+        var speedInString = speed.get(1);
+        if (speedInString.equalsIgnoreCase("kb")) {
+            realSpeed *= 1000;
+        } else if (speedInString.equalsIgnoreCase("mb")) {
+            realSpeed *= 1000000;
+        }
+        return realSpeed;
+    }
+
+    public static void main(String[] args) {
+        logic(args);
+    }
+
+    /**
+     * -url {url value} -t {target path} -n {name} -s {num} {kof}
+     */
+    private static class Config {
+
+        public Map<String, List<String>> processingArgs(String[] args) {
+            return ArgsLambda.build()
+                    .add("-url", arg -> arg.contains("https://"))
+                    .add("-t", ars -> true)
+                    .add("-n", ars -> true)
+                    .add("-s", List.of(
+                            arg -> arg.matches("^\\d*$"),
+                            arg -> arg.equalsIgnoreCase("b") || arg.equalsIgnoreCase("kb")
+                                    || arg.matches("mb")))
+                    .load(args)
+                    .print()
+                    .runToMap();
+        }
+    }
 }
